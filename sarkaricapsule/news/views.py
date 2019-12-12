77bd2fb04331e413a9c1ec0d93db4a256a5dd1b8
Savinay Kumar <savinaykumar@outlook.com>
2019-12-12 16:36:16 +0530

    migration_to_diff_server

--- sarkaricapsule/news/views.py
+++ sarkaricapsule/news/views.py
@@ -1,3 +1,16 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView, View
 
-# Create your views here.
+from .models import News
+
+
+class NewsHome(View):
+
+    def get(self, request, *args, **kwargs):
+        
+        news = News.objects.all()
+        
+        return render(request, 'news/index.html', {
+            'news': news
+        })
